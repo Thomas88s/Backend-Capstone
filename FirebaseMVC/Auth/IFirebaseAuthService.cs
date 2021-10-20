ed7d3d1d727d484248e1435a192f279774e3b012
Thomas Witherow <twitherow@bellsouth.net>
2021-10-20 14:26:24 -0500

    initial

---
+++ FirebaseMVC/Auth/IFirebaseAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using StonkMarket.Auth.Models;
+
+namespace StonkMarket.Auth
+{
+    public interface IFirebaseAuthService
+    {
+        Task<FirebaseUser> Login(Credentials credentials);
+        Task<FirebaseUser> Register(Registration registration);
+    }
+}
---
+++ FirebaseMVC/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,11 @@
+﻿using StonkMarket.Models;
+
+namespace StonkMarket.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        UserProfile GetByFirebaseUserId(string firebaseUserId);
+        UserProfile GetById(int id);
+    }
+}
