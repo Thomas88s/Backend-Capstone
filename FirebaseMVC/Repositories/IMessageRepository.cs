12f7f103cc60341a4761ca22b7cf4c2a1076e22e
Thomas Witherow <twitherow@bellsouth.net>
2021-10-25 13:56:09 -0500

    added messages crud

---
+++ FirebaseMVC/Repositories/IMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using StonkMarket.Models;
+using System.Collections.Generic;
+
+namespace StonkMarket.Repositories
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(int messageId);
+        List<Message> GetAllMessages();
+        Message GetMessageById(int id);
+        void UpdateMessage(Message message);
+    }
+}
