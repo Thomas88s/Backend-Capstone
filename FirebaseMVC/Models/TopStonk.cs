51b2e63003b34f827bd4fc2af755e1a764fa4396
Thomas Witherow <twitherow@bellsouth.net>
2021-10-25 10:42:57 -0500

    added top stonks

---
+++ FirebaseMVC/Models/TopStonk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StonkMarket.Models
+{
+    public class TopStonk
+    {
+        public int Id { get; set; }
+        public int StonkId { get; set; }
+        public int UserId { get; set; }
+        public int PercentageIncrease { get; set; }
+        public bool TopPerformer { get; set; }
+    }
+}
---
+++ FirebaseMVC/Repositories/ITopStonkRepository.cs
@@ -0,0 +1,14 @@
+﻿using StonkMarket.Models;
+using System.Collections.Generic;
+
+namespace StonkMarket.Repositories
+{
+    public interface ITopStonkRepository
+    {
+        void Add(TopStonk topStonk);
+        void Delete(int topStonkId);
+        List<TopStonk> GetAllTopStonks();
+        TopStonk GetTopStonkById(int id);
+        void Update(TopStonk topStonk);
+    }
+}
